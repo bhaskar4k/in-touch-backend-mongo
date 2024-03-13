57e7f6b70bfd68ba8c7852fb27405f762db7b446
Bhaskar Nandy <bhaskarndy@gmail.com>
2024-03-14 01:49:55 +0530

    Commit

--- api/allAPI.js
+++ api/allAPI.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { get_all_post, add_post, delete_post } = require('../controllers/PostController');
+const { get_post_count, get_all_post, add_post, delete_post } = require('../controllers/PostController');
 const router = express.Router();
 
 router.post('/add_post', add_post)
 router.post('/delete_post', delete_post)
 router.get('/get_all_post/:username', get_all_post)
+router.get('/get_post_count/:username', get_post_count)
 
 module.exports = router;
--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,25 @@
 const PostModel = require('../models/PostModel');
 
+async function get_post_count(req, res) {
+    const { username } = req.params;
+
+    try {
+        const count = await PostModel.countDocuments({ username: username });
+        res.status(200).json({ count: count });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 async function get_all_post(req, res) {
     const { username } = req.params;
 
     try {
-        const post = await PostModel.find({ username }).sort({ createdAt: -1 });
+        //const post = await PostModel.find({ username }).sort({ createdAt: -1 });
+        const post = await PostModel.find({ username })
+            .sort({ createdAt: -1 }) // Sort by createdAt in descending order
+            .skip(1) // Skip the first 9 posts
+            .limit(2);
         res.status(200).json(post);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -25,7 +40,6 @@ async function add_post(req, res) {
 async function delete_post(req, res) {
     try {
         const { post_id } = req.body;
-        console.log(post_id)
 
         const result = await PostModel.deleteOne({ post_id: post_id });
         res.status(200).json(result);
@@ -34,4 +48,4 @@ async function delete_post(req, res) {
     }
 }
 
-module.exports = { get_all_post, add_post, delete_post }
+module.exports = { get_all_post, add_post, delete_post, get_post_count }
